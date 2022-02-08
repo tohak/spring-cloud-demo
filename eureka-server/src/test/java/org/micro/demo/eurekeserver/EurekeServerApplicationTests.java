b49510577e2b4e4e453902eb584813ff49a76b0e
Anton <tohak199>
2022-02-09 00:00:19 +0200

    добавлен eureka сервер и клиент

---
+++ eureka-server/src/test/java/org/micro/demo/eurekeserver/EurekeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.micro.demo.eurekeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
